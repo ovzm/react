2c278d95e38dd931ab76f2cd560b27d65aed5bbe
Victor Zawacki Monteiro <victorzawacki9@gmail.com>
2026-08-21 16:36:34 -0300

    feat: captura dados do cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,54 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const user = {
-      name: formData.get("name"),
-      email: formData.get("email"),
-      age: formData.get("age"),
+    // Event é usado para capturar o evento de envio do formulário - vai chegar as informações do formulário
+    event.preventDefault(); // Evita o envio do formulário e o recarregamento da página
+
+    const newUser = {
+      // Cria um novo objeto de usuário com as informações do formulário
+      name: name,
+      email: email,
+      age: age,
     };
-    console.log(user);
+
+    setUsers([...users, newUser]); // Adiciona um novo usuário ao array de usuários
+
+    console.log(users);
   }
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const [users, setUsers] = useState([]);
 
   return (
     <div className="App">
       <h1>Cadastro de Usuários</h1>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Nome" />
-        <input type="email" name="email" placeholder="Email" />
-        <input type="number" name="age" placeholder="Idade" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)} // assim que pega as informações do input, ele vai atualizar o estado do name
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="number"
+          name="age"
+          placeholder="Idade"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
         <button type="submit">Cadastrar</button>
       </form>
       <div className="user-list">
